9cd3168e2d3d28e1799b3e00ea58955fd3d69c14
Serhii Krimets <75704401+Sergio1004@users.noreply.github.com>
2021-08-25 16:18:56 +0300

    add Statistics

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import './App.css';
 import Profile from './components/Profile/Profile';
+import Statistics from './components/Statistics/Statistics';
 
 import user from './data/user.json';
+import statisticalData from './data/statistical-data.json';
 
 function App() {
   return (
@@ -13,6 +15,7 @@ function App() {
         avatar={user.avatar}
         stats={user.stats}
       />
+      <Statistics title="Upload stats" stats={statisticalData} />
     </div>
   );
 }
