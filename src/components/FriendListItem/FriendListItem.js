4cc319ab65fd0b6d3f4695cfa4cb2118b6926902
Serhii Krimets <krimets.s88@gmail.com>
2021-10-31 23:44:38 +0200

    update images

--- src/components/FriendListItem/FriendListItem.js
+++ src/components/FriendListItem/FriendListItem.js
@@ -4,7 +4,7 @@ import s from './FriendListItem.module.css';
 function FriendListItem({ avatar, name, isOnline }) {
   return (
     <li className={s.item}>
-      <span className={isOnline ? s.isOnline : s.isOffline}></span>
+      <span className={isOnline ? s.isOnline : s.isOffline} />
       <img className={s.avatar} src={avatar} alt={name} width="48" />
       <p className={s.name}>{name}</p>
     </li>
